ac2051f0e1445b6215ba598600dd99041d5343f5
Келемен Ярослав bc2025-4 <yaroslav.project1@gmail.com>
2026-09-09 12:23:09 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Келемен Ярослав, група ФЕІ-33с");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Склад (товари, партії, залишки, переміщення)");
